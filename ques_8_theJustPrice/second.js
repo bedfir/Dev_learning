e6abc88db932f4d06bc9bc81dda3802c6b3d0732
Firas BEDOUI <firas-bedoui@hotmail.fr>
2021-07-27 15:14:35 +0200

    projet fini

--- ques_8_theJustPrice/second.js
+++ ques_8_theJustPrice/second.js
@@ -1,5 +1,6 @@
 const attempt = document.querySelector('#attempt');
 const message = document.querySelector('#message');
+const videoElem = document.querySelector('#video');
 const btn = document.querySelector('#btn');
 let attemptNb = 10;
 let priceMystere = Math.floor(Math.random() * 100) + 1;
@@ -43,7 +44,6 @@ attempt.textContent = `Il vous reste ${attemptNb} tentatives`
 
 
 
-
 const verifierProposition = (e) => {
     const inputPrice = document.querySelector('#inputPrice').value;
     if (attemptNb == 0) {
@@ -65,8 +65,17 @@ const verifierProposition = (e) => {
             attempt.textContent = `Il vous reste ${attemptNb} tentatives`
         }
         if (inputPrice == priceMystere) {
+            let node = document.createElement('iframe');
+            node.setAttribute('id', 'Player');
+            node.setAttribute('title', 'YouTube video player');
+            node.setAttribute('frameborder', '0');
+            node.setAttribute('allowfullscreen', '1');
+            node.setAttribute('allow', 'autoplay');
+            node.setAttribute('width', '640');
+            node.setAttribute('height', '264');
+            node.setAttribute('src',"https://www.youtube.com/embed/4Csi0U8c-LA");
+            document.getElementById('video').append(node);
             message.textContent = 'Bravo vous avez gagné !';
-            message.innerHTML = `<video width="320" height="240" autoplay> <source src="https://www.youtube.com/watch?v=4Csi0U8c-LA" type="video/mp4"></video>`
             attemptNb--;
             attempt.textContent = `En ${attemptNb} tentatives`
             btn.disabled = true;
