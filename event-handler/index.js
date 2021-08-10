968b9b293aba9408d5ca4ac27797330d5fa0e519
Firas BEDOUI <firas-bedoui@hotmail.fr>
2021-08-10 14:07:59 +0200

    event - target

---
+++ event-handler/index.js
@@ -0,0 +1,15 @@
+const btn = document.querySelector('button');
+
+function random(number) {
+    return Math.floor(Math.random() * number);
+}
+
+function bgChange(e) {
+    const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    // document.body.style.backgroundColor = rndCol;
+    e.target.style.backgroundColor = rndCol;
+    console.log(e);
+}
+
+// btn.onclick = bgChange;
+btn.addEventListener('click', bgChange);
