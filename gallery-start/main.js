8349b7a7995832a3a659bb2a0bf4add858d8917e
Firas BEDOUI <firas-bedoui@hotmail.fr>
2021-08-16 11:27:03 +0200

    finally

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -8,10 +8,24 @@ const overlay = document.querySelector('.overlay');
 
 for (let index = 1; index < 6; index++) {
     const newImage = document.createElement('img');
-    newImage.setAttribute('src', "./images/pic" + [index] +".jpg");
+    newImage.setAttribute('src', "./images/pic" + [index] + ".jpg");
     thumbBar.appendChild(newImage);
+    newImage.onclick = function(e) {
+        displayedImage.src = e.target.src;
+    }
 }
 
 
-
 /* Wiring up the Darken/Lighten button */
+btn.onclick = function () {
+    let attribute = btn.getAttribute('class');
+    if (attribute === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";
+    } else {
+        btn.setAttribute('class', 'dark');
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgba(0,0,0,0)";
+    }
+}
