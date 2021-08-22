d552cbfa044873c75bf76dca23f671c0718513e2
Firas BEDOUI <firas-bedoui@hotmail.fr>
2021-08-22 23:28:39 +0200

    Finally bouncing balls

--- objects/object_building_practice/index.js
+++ objects/object_building_practice/index.js
@@ -13,7 +13,8 @@ function random(min, max) {
     return num;
 }
 
-//Create constructor ball model
+// define Ball constructor
+
 function Ball(x, y, velX, velY, color, size) {
     this.x = x;
     this.y = y;
@@ -23,50 +24,40 @@ function Ball(x, y, velX, velY, color, size) {
     this.size = size;
 }
 
+// define ball draw method
 
-//--------------All methodes
-//Create methode to the Ball()s prototype
-//draw methode
 Ball.prototype.draw = function () {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
-}
+};
 
-//Create a new ball instance
-/* let testBall = new Ball(50, 100, 4, 4, 'blue', 10);
-testBall.x
-testBall.size
-testBall.color
-testBall.draw() */
+// define ball update method
 
-//Add update() methode to the ball prototype
-//check whether the ball has reached the edge of the canvas
 Ball.prototype.update = function () {
-    //if the x coordinate is greater than the width of the canvas (the ball is going off the right edge).
     if ((this.x + this.size) >= width) {
         this.velX = -(this.velX);
     }
-    //if the x coordinate is smaller than 0 (the ball is going off the left edge).
+
     if ((this.x - this.size) <= 0) {
         this.velX = -(this.velX);
     }
-    // if the y coordinate is greater than the height of the canvas (the ball is going off the bottom edge).
+
     if ((this.y + this.size) >= height) {
         this.velY = -(this.velY);
     }
-    // if the y coordinate is smaller than 0 (the ball is going off the top edge).
+
     if ((this.y - this.size) <= 0) {
         this.velY = -(this.velY);
     }
 
-    // add the velX value to the x coordinate, and the velY value to the y coordinate
     this.x += this.velX;
     this.y += this.velY;
-}
+};
 
-//Adding collision detection
+// define ball collision detection
+
 Ball.prototype.collisionDetect = function () {
     for (let j = 0; j < balls.length; j++) {
         if (!(this === balls[j])) {
@@ -79,18 +70,17 @@ Ball.prototype.collisionDetect = function () {
             }
         }
     }
-}
+};
 
+// define array to store balls and populate it
 
-
-//-------------Animating the ball
 let balls = [];
-//creates a new instance of our Ball()
+
 while (balls.length < 25) {
-    let size = random(10, 20);
+    const size = random(10, 20);
     let ball = new Ball(
         // ball position always drawn at least one ball width
-        // away from the edge of the canvas, to avoid drawing errors
+        // away from the adge of the canvas, to avoid drawing errors
         random(0 + size, width - size),
         random(0 + size, height - size),
         random(-7, 7),
@@ -98,13 +88,13 @@ while (balls.length < 25) {
         'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
         size
     );
-
     balls.push(ball);
 }
 
-//update the information in the program and then render the resulting view on each frame of the animation
+// define loop that keeps drawing the scene constantly
+
 function loop() {
-    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.fillStyle = 'rgba(0,0,0,0.25)';
     ctx.fillRect(0, 0, width, height);
 
     for (let i = 0; i < balls.length; i++) {
@@ -113,7 +103,6 @@ function loop() {
         balls[i].collisionDetect();
     }
 
-    //the function is calling itself every time it runs, so it runs over and over again.
     requestAnimationFrame(loop);
 }
 
