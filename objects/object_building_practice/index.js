4ccfabc54a421ce4703bdb9738c241fff66a520e
Firas BEDOUI <firas-bedoui@hotmail.fr>
2021-08-30 17:53:35 +0200

    work finish

--- objects/object_building_practice/index.js
+++ objects/object_building_practice/index.js
@@ -1,3 +1,8 @@
+// define variable for ball count paragraph
+
+const para = document.querySelector('p');
+let count = 0;
+
 // setup canvas
 
 const canvas = document.querySelector('canvas');
@@ -13,21 +18,32 @@ function random(min, max) {
     return num;
 }
 
-// define Ball constructor
+// define Shape constructor
 
-function Ball(x, y, velX, velY, color, size) {
+function Shape(x, y, velX, velY, exists) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+    this.exists = exists;
+}
+
+// define Ball constructor, inheriting from Shape
+
+function Ball(x, y, velX, velY, color, size, exists) {
+    Shape.call(this, x, y, velX, velY, exists);
+
     this.color = color;
     this.size = size;
 }
+Ball.prototype = Object.create(Shape.prototype);
+Ball.prototype.constructor = Ball;
 
 // define ball draw method
 
 Ball.prototype.draw = function () {
     ctx.beginPath();
+    ctx.lineWidth = 3;
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
     ctx.fill();
@@ -52,9 +68,12 @@ Ball.prototype.update = function () {
         this.velY = -(this.velY);
     }
 
+
+
     this.x += this.velX;
     this.y += this.velY;
 };
+
 
 // define ball collision detection
 
@@ -72,6 +91,93 @@ Ball.prototype.collisionDetect = function () {
     }
 };
 
+
+// define evilCircle constructor
+
+function EvilCircle(x, y, exists) {
+    Shape.call(this, x, y, 20, 20, exists);
+
+    this.color = 'white';
+    this.size = 10;
+}
+
+EvilCircle.prototype = Object.create(Shape.prototype);
+EvilCircle.prototype.constructor = EvilCircle;
+
+// define EvilCircle checkBounds method
+
+EvilCircle.prototype.checkBounds = function () {
+    if ((this.x + this.size) >= width) {
+        this.x -= this.size;
+    }
+
+    if ((this.x - this.size) <= 0) {
+        this.x += this.size;
+    }
+
+    if ((this.y + this.size) >= height) {
+        this.y -= this.size;
+    }
+
+    if ((this.y - this.size) <= 0) {
+        this.y += this.size;
+    }
+};
+
+
+// define EvilCircle draw methode
+
+EvilCircle.prototype.draw = function () {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+}
+
+// define EvilCircle setControls method
+
+EvilCircle.prototype.setControls = function () {
+    let _this = this;
+    window.onkeydown = function (e) {
+        if (e.keyCode === 37) {
+            _this.x -= _this.velX;
+        } else if (e.keyCode === 39) {
+            _this.x += _this.velX;
+        } else if (e.keyCode === 38) {
+            _this.y -= _this.velY;
+        } else if (e.keyCode === 40) {
+            _this.y += _this.velY;
+        }
+    }
+}
+
+// define EvilCircle collision detection
+
+EvilCircle.prototype.collisionDetect = function () {
+    for (let j = 0; j < balls.length; j++) {
+        if (balls[j].exists) {
+            const dx = this.x - balls[j].x;
+            const dy = this.y - balls[j].y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + balls[j].size) {
+                balls[j].exists = false;
+                count--;
+                para.textContent = 'Ball count: ' + count;
+            }
+        }
+    }
+}
+
+
+
+// instance evil
+let evil = new EvilCircle(random(0, width), random(0, height), true);
+evil.setControls();
+
+
+
 // define array to store balls and populate it
 
 let balls = [];
@@ -86,9 +192,12 @@ while (balls.length < 25) {
         random(-7, 7),
         random(-7, 7),
         'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
-        size
+        size,
+        true
     );
     balls.push(ball);
+    count++;
+    para.textContent = 'Ball count: ' + count;
 }
 
 // define loop that keeps drawing the scene constantly
@@ -98,11 +207,15 @@ function loop() {
     ctx.fillRect(0, 0, width, height);
 
     for (let i = 0; i < balls.length; i++) {
-        balls[i].draw();
-        balls[i].update();
-        balls[i].collisionDetect();
+        if (balls[i].exists) {
+            balls[i].draw();
+            balls[i].update();
+            balls[i].collisionDetect();
+        }
+        evil.draw();
+        evil.checkBounds();
+        evil.collisionDetect();
     }
-
     requestAnimationFrame(loop);
 }
 
